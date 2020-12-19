5eb13738c02c7aff974b82a58a2e9e54d3705603
Josh Batchelor <josh_batch@live.com.au>
2020-12-19 17:49:57 +1100

    Update TSconfig to pass through build mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,56 +5,57 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 const PUBLIC_URL = process.env.PUBLIC_URL || '';
 
-const config = {
-  context: path.resolve(__dirname, 'client/src/'),
-  entry: './index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist', 'client'),
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './public'),
-    contentBasePublicPath: '/',
-    stats: 'errors-only',
-    port: 8000,
-    compress: true,
-    historyApiFallback: true,
-    proxy: [
-      {
-        context: ['/api', '/media', '/socket.io'],
-        target: 'http://localhost:9000',
-      },
+module.exports = (env, options) => {
+  const config = {
+    context: path.resolve(__dirname, 'client/src/'),
+    entry: './index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist', 'client'),
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, './public'),
+      contentBasePublicPath: '/',
+      stats: 'errors-only',
+      port: 8000,
+      compress: true,
+      historyApiFallback: true,
+      proxy: [
+        {
+          context: ['/api', '/media', '/socket.io'],
+          target: 'http://localhost:9000',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [{ test: /\.tsx?$/, loader: 'ts-loader', exclude: '/node_modules/' }],
+    },
+    plugins: [
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: options.mode,
+        PUBLIC_URL: 'http://localhost:8000',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: '../public', to: './' }],
+      }),
+      new HtmlWebpackPlugin({
+        template: '../public/index.ejs',
+        filename: './index.html',
+        PUBLIC_URL: PUBLIC_URL,
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [{ test: /\.tsx?$/, loader: 'ts-loader', exclude: '/node_modules/' }],
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development',
-      PUBLIC_URL: 'http://localhost:8000',
-    }),
-    new CopyPlugin({
-      patterns: [{ from: '../public', to: './' }],
-    }),
-    new HtmlWebpackPlugin({
-      template: '../public/index.ejs',
-      filename: './index.html',
-      PUBLIC_URL: PUBLIC_URL,
-    }),
-  ],
+  };
+
+  const envVarMap = {
+    APP_BASE_PATH: '',
+  };
+
+  if (process.env.PROD) {
+    envVarMap['APP_BASE_PATH'] = '';
+  }
+
+  config.plugins.push(new webpack.EnvironmentPlugin(envVarMap));
+  return config;
 };
-
-const envVarMap = {
-  APP_BASE_PATH: '',
-};
-
-if (process.env.PROD) {
-  envVarMap['APP_BASE_PATH'] = '';
-}
-
-config.plugins.push(new webpack.EnvironmentPlugin(envVarMap));
-
-module.exports = config;
